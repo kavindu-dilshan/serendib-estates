21e6100059a8c5fc86da3f7c03cd0242bc9283ba
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-01-14 11:13:39 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux'
 
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
+
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,15 +18,15 @@ export default function Header() {
             <input type='text' placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-600'/>
         </form>
-        <ul className='flex gap-4 font-semibold'>
+        <ul className='flex gap-4 font-semibold items-center'>
             <Link to='/'>
             <li className='hidden sm:inline text-slate-600 hover:text-slate-900'>Home</li>
             </Link>
             <Link to='/about'>
             <li className='hidden sm:inline text-slate-600 hover:text-slate-900'>About</li>
             </Link>
-            <Link to='/sign-in'>
-            <li className=' text-slate-600 hover:text-slate-900'>Sign In</li>
+            <Link to='/profile'>
+              {currentUser ? (<img className='rounded-full h-10 w-10 object-cover' src={currentUser.avatar} alt="Profile" />) : <li className=' text-slate-600 hover:text-slate-900'>Sign In</li>}
             </Link>
         </ul>
         </div>
