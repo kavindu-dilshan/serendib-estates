434a81e2e5e11233bbaf6c173f9e2d50eb5138af
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-01-19 14:22:18 +0530

    add show more listings functionality

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -16,6 +16,7 @@ export default function ListingItem({listing}) {
           <p className='text-sm text-gray-500 line-clamp-2 mt-2'>{listing.description}</p>
           <p>
           {listing.offer && (<p className=' text-green-700 mt-2 font-bold'>LKR {+listing.regularPrice - +listing.discountPrice} {listing.type === 'rent' && '/ Month'}</p>)}
+          {!listing.offer && (<p className=' text-green-700 mt-2 font-bold'>LKR {+listing.regularPrice} {listing.type === 'rent' && '/ Month'}</p>)}
           </p>
           <ul className='text-slate-700 font-semibold text-xs flex flex-wrap items-center gap-4 sm:gap-6'>
             <li className='flex items-center gap-1 whitespace-nowrap'>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,8 +15,7 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-
-    console.log(listings);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
@@ -42,9 +41,15 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length > 7) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         }
@@ -87,6 +92,20 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    };
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length < 8) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     }
 
   return (
@@ -152,6 +171,10 @@ export default function Search() {
                 {!loading && listings && listings.map((listing) => (
                     <ListingItem key={listing._id} listing={listing}/>
                 ))}
+
+                {showMore && (
+                    <button onClick={onShowMoreClick} className='text-green-700 p-7 hover:text-green-900 font-bold text-center w-full'>Show more</button>
+                )}
             </div>
         </div>
     </div>
